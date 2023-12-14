3d52d8be7dbaf6ed09600c37c2daba4b7819bcd5
imwisdom <kjihye0340@naver.com>
2023-12-14 16:48:50 +0900

    initial commit of board-fe

---
+++ board/board-fe/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MenuFrame from './components/MenuFrame';
+
+function App() {
+  return (
+    <div className="App">
+      <MenuFrame></MenuFrame>
+    </div>
+  );
+}
+
+export default App;
