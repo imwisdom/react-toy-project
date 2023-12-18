0b240db1f10862af34508e11ea501a37b649337f
imwisdom <kjihye0340@naver.com>
2023-12-18 16:35:27 +0900

    feature: 회원가입 form 생성

--- board/board-fe/src/pages/SignUp.tsx
+++ board/board-fe/src/pages/SignUp.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import MenuFrame from '../components/MenuFrame';
+import SignUpForm from '../components/SignUpForm';
 
 const SignUp: React.FC = () => {
     return (
         <MenuFrame pageTitle='signUp'>
-            <div>
-                <p>회원가입화면</p>
-            </div>
+            <SignUpForm />
         </MenuFrame>
     )
 };
