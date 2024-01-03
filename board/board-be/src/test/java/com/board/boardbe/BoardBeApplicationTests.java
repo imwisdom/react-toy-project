1da3ca0dbb54ac28266927dd8c76fcea88db395b
imwisdom <kjihye0340@naver.com>
2024-01-03 17:13:27 +0900

    initial commit of board-be

---
+++ board/board-be/src/test/java/com/board/boardbe/BoardBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.boardbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
